991a3e558e2b60d224a7eceea6311da91376ce57
Chiramet Phong Penglerd <Phong@hotmail.ch>
2017-11-25 00:35:43 +0100

    Initial commit

---
+++ src/main/java/ch/chiramet/tagtion/TagtionApplication.java
@@ -0,0 +1,12 @@
+package ch.chiramet.tagtion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagtionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagtionApplication.class, args);
+	}
+}
---
+++ src/test/java/ch/chiramet/tagtion/TagtionApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.chiramet.tagtion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TagtionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
